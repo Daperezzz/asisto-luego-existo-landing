33b9bd7f0c8d3745c2a2b7ea71559518dd2ec293
Daperezzz <danielalejandr88@gmail.com>
2024-11-22 00:56:14 -0300

    "Añadido atributo download para configurar nombre del APK"

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const downloadButton = document.getElementById('download-button');
     const messageElement = document.getElementById('device-message');
 
-    // Detecta Android
+    // Detecta si el dispositivo es Android
     if (/Android/i.test(navigator.userAgent)) {
         downloadButton.href = "https://github.com/daperezzz/asisto-luego-existo-landing/releases/download/v1.0.0/app-release.apk";
-        downloadButton.classList.remove('hidden');
+        downloadButton.classList.remove('hidden'); // Muestra el botón
         messageElement.textContent = 'Estás en un dispositivo Android. Descarga la app ahora.';
     } else {
-        messageElement.textContent = 'Estás en un dispositivo que no es Android.';
+        messageElement.textContent = 'Este enlace solo está disponible para dispositivos Android.';
     }
 });
